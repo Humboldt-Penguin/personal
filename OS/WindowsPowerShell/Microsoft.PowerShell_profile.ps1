5384b035e29f3fcfa1a7f1d584db86256ba4c202
Zain Eris Kamal <zain.eris.kamal@gmail.com>
2023-04-29 02:18:26 -0400

    os/powershell -- linux-like ls command for powershell; better paths

--- OS/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
+++ OS/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,5 +1,6 @@
 function gotomars {
-	cd D:\sync\01_Research\Mars_Magnetics
+	# cd D:\sync\01_Research\Mars_Magnetics # if on D:sync partition
+	cd C:\Users\Eris\Documents\sync_local\01_Research\Mars_Magnetics # if on local
 }
 
 function exportenv_nocomment {
@@ -10,9 +11,21 @@ function exportenv_nocomment {
     Write-Host "Exported Mamba environment to $filename"
 }
 
-function launchmars {
+function launchmars1 {
 	gotomars
 	mamba activate mars1
+	jupyter lab  --ContentsManager.allow_hidden=True
+}
+
+function launchmars2 {
+	gotomars
+	mamba activate mars2
+	jupyter lab  --ContentsManager.allow_hidden=True
+}
+
+function launchmars3 {
+	gotomars
+	mamba activate mars3
 	jupyter lab  --ContentsManager.allow_hidden=True
 }
 
@@ -39,4 +52,92 @@ function exportenv {
     $filename = "environment--$date--$envName.yml"
 	$output | Out-File -FilePath $filename -Encoding UTF8
 	
+}
+
+
+# Move prompt input to newline
+function prompt {
+	"PS " + (Get-Location) + "\`n> "
+	
+	# more examples available here: https://superuser.com/questions/446827/configure-windows-powershell-to-display-only-the-current-folder-name-in-the-shel
+}
+
+# Better "ls"
+
+Remove-Item Alias:ls
+
+function ls {
+
+	Get-ChildItem | Format-Table -HideTableHeaders -Property `
+		@{ `
+			Name="Name"; `
+			Expression={
+				if($_.PSIsContainer){
+					$_.Name + ("/       ")
+				}
+				else{
+					$_.Name + ("       ")
+				}
+			}; `
+			Width=50
+		},
+		@{ `
+			Name="LastWriteTime"; `
+			Expression={
+				[string]($_.LastWriteTime) + ("       ")
+			}; `
+			Width=30
+		},
+		@{ `
+			Name="Length (MB)"; `
+			Expression={
+				[string]([math]::Round($_.Length/1MB)) + " MB"
+			}
+		}
+
+
+
+	#############################################################
+	## other options
+	
+	
+	<#
+	Get-ChildItem | Format-Table -HideTableHeaders -Property `
+		@{ `
+			Name="Name"; `
+			Expression={
+				if($_.PSIsContainer){
+					$_.Name + ("/       ")
+				}
+				else{
+					$_.Name + ("       ")
+				}
+			}; `
+			Width=50
+		},
+		@{ `
+			Name="LastWriteTime"; `
+			Expression={
+				[string]($_.LastWriteTime) + ("       ")
+			}; `
+			Width=30
+		},
+		@{ `
+			Name="Length (MB)"; `
+			Expression={
+				[string]([math]::Round($_.Length/1MB)) + " MB"
+			}
+		}
+	#>
+	
+	
+	<#
+	Get-ChildItem | Format-Table -HideTableHeaders -Property @{Name="Name";Expression={$_.Name};Width=40}, @{Name="LastWriteTime";Expression={$_.LastWriteTime};Width=25}, @{Name="Length (MB)";Expression={[math]::Round($_.Length/1MB)};Width=15}
+	#>
+
+
+	<#
+	Get-ChildItem | Format-Table -HideTableHeaders -Property @{Name="Name";Expression={$_.Name + ("   ")};Width=40}, @{Name="LastWriteTime";Expression={$_.LastWriteTime};Width=25}, @{Name="Length (MB)";Expression={[math]::Round($_.Length/1MB)};Width=15} -Wrap
+	#>
+
 }
